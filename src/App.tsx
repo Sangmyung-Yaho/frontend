c0d61a5122365852e15ee56e13110095a03025aa
Skysny12 <127105223+Skysny12@users.noreply.github.com>
2026-08-12 23:48:10 +0900

    fix:루트 페이지를 랜딩화면으로 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
-  return <HomePage />;
+  return <LandingPage />;
 }
 
 export default App;
