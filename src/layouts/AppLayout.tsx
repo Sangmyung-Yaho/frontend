dfa4604570071b73d37a288aac42f6c37c570dd5
Skysny12 <127105223+Skysny12@users.noreply.github.com>
2026-08-19 00:42:15 +0900

    fix: 데스크톱 앱 너비 393px 복구

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
   return (
-    <div className="relative mx-auto min-h-dvh w-screen max-w-none overflow-x-hidden bg-background px-4 lg:max-w-[393px] [&>main]:min-h-dvh">
+    <div className="relative mx-auto min-h-dvh w-screen max-w-none overflow-x-hidden bg-background px-4 sm:max-w-[393px] [&>main]:min-h-dvh">
       <Outlet />
     </div>
   );
